4ef2b35a2fd306572f65520c42f0f9ebb0e00ed6
Mahdi Majdian <MajdianMahdi@gmail.com>
2022-01-22 17:03:11 +0330

    configure tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 const App = (): React.ReactElement => {
-  return <h1>Hello React!</h1>;
+  return <h1 className='mt-8 font-medium text-center text-6xl text-purple-600'>Hello React!</h1>;
 };
 
 export default App;
